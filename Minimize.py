90c10cf92974841c711a74139256c54645473e91
Zhongtian Dong <78445813+ZhongtianD@users.noreply.github.com>
2021-03-30 15:43:28 -0500

    Update Minimize.py

--- Minimize.py
+++ Minimize.py
@@ -1,5 +1,9 @@
 # imports of external packages to use in our code
 from scipy.optimize import minimize
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D  
+import matplotlib.pyplot as plt
+from matplotlib import cm
 #from iminuit import Minuit
 
 
@@ -12,7 +16,28 @@ if __name__ == "__main__":
     #It minimizes the  function 
     res = minimize(fcn,(0,0), tol=1e-6)
     
-    print(res.x)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = np.arange(0.0, 3.0, 0.05)
+    y = np.arange(0.0, 6.0, 0.05)
+    X, Y = np.meshgrid(x, y)
+    zs = np.array(fcn((np.ravel(X), np.ravel(Y))))
+    Z = zs.reshape(X.shape)
+    ax.plot_surface(X, Y, Z,cmap=cm.coolwarm)
+    ax.scatter(res.x[0], res.x[1], res.fun, c = 'r', marker = 'X')
+    ax.scatter(2, 4, 0, c = 'r', marker = 'X', label='minimum')
+    ax.set_xlabel('X ')
+    ax.set_ylabel('Y ')
+    ax.set_zlabel('Function value')
+    ax.legend()
+
+    ax.view_init( azim=30)
+
+    plt.show()
+    plt.save('minimization.png')
+    
+    print('The parameter is given by x='+res.x[0]+' and y='+res.x[1])
+    print('function was evaluated '+res.nfev+'times')
     
     
     #Using Minuit
